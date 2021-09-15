e0320cf92b2a4f99e622ea433c0332e61b9af1d5
rohitgr7 <rohitgr1998@gmail.com>
2021-09-16 00:33:25 +0530

    review comments

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -990,7 +990,8 @@ class Trainer(
         # TRAIN
         # ----------------------------
         # reset logger connector
-        self.logger_connector.reset(trainer_metrics=True)
+        self.logger_connector.reset_results()
+        self.logger_connector.reset_metrics()
 
         # hook
         if self.state.fn == TrainerFn.FITTING:
@@ -1169,7 +1170,8 @@ class Trainer(
             self.sanity_checking = True
 
             # reset validation metrics
-            self.logger_connector.reset(trainer_metrics=True)
+            self.logger_connector.reset_results()
+            self.logger_connector.reset_metrics()
 
             self.call_hook("on_sanity_check_start")
 
@@ -1183,7 +1185,8 @@ class Trainer(
             self.call_hook("on_sanity_check_end")
 
             # reset validation metrics
-            self.logger_connector.reset(trainer_metrics=True)
+            self.logger_connector.reset_results()
+            self.logger_connector.reset_metrics()
 
             # reset the seed to what it was before sanity check
             # prevents sanity check to affect random sampling in training
